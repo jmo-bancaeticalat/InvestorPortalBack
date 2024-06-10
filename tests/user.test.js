62cd857dac0d1e24a8e06b63064ae758ef4db88f
jmo-bancaeticalat <joberreuter@bancaeticalat.com>
2024-06-10 14:40:17 -0400

    Update test getUserPostgres

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,20 +1,46 @@
 const server = require('../index.js');
 const request = require('supertest'); //SuperTest is used to test the Express application without starting the server 
+const { validateNumeric } = require("../utils/validation")
 
 //Test endpoint getUserPostgres - return all users
 describe("GET /api/v1/getUserPostgres", () => {
   //Testing ok status code
-  test("Should respond with a 200 status code", async () => {
-    const response = await request(server).get("/api/v1/getUserPostgres").send();
-
+  test("Should respond with a 200 status code and user information", async () => {
+    const response = await request(server).get("/api/v1/getUserPostgres?id_user=1").send();
+    expect(response.body[0]).toHaveProperty("id_user");
+    expect(response.body[0]).toHaveProperty("profile");
+    expect(response.body[0].profile).toHaveProperty("name");
     expect(response.statusCode).toBe(200);
   });
 
   test("Should respond with an array", async ()=>{
-    const response = await request(server).get("/api/v1/getUserPostgres").send();
-
+    const response = await request(server).get("/api/v1/getUserPostgres?id_user=1").send();
     expect(response.body).toBeInstanceOf(Array);
   });
+
+  // Prueba si se manejan los casos de error adecuadamente
+  test("Should respond with 400 if user ID is missing", async () => {
+    const response = await request(server).get("/api/v1/getUserPostgres?id_user=").send();
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("error", "User ID is required");
+  });
+
+  test("Should respond with 400 if user ID is invalid", async () => {
+    const response = await request(server).get("/api/v1/getUserPostgres?id_user=abc").send(); // Proporciona un ID de usuario no numérico
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("error", "Invalid User ID");
+  });
+
+  test("Should respond with 404 if user is not found", async () => {
+    const response = await request(server).get("/api/v1/getUserPostgres?id_user=999").send(); // Proporciona un ID de usuario que no existe en tus datos de prueba
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toHaveProperty("error", "User not found");
+  });
+
+
 });
 
 //!Comment for failure jest with jenkins
