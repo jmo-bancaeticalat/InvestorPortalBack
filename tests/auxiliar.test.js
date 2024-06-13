72ccda9796840d00ab55289035358949989c7185
jmo-bancaeticalat <joberreuter@bancaeticalat.com>
2024-06-13 11:44:46 -0400

    Updated auxiliar.test, added new test getEducationalLevel

--- tests/auxiliar.test.js
+++ tests/auxiliar.test.js
@@ -75,7 +75,40 @@ describe("GET /api/v1/getOccupations", () => {
 
 });
 
+describe("GET /api/v1/getEducationalLevel", () => {
 
+    test("Should respond with 200 status code and the filtered educational levels", async () => {
+        const response = await request(server)
+            .get("/api/v1/getEducationalLevel")
+            .query({ id_country: 39 });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty("educationalLevels");
+        expect(Array.isArray(response.body.educationalLevels)).toBe(true);
+        // Additional checks to ensure the filtered results match the provided country ID
+        response.body.educationalLevels.forEach(level => {
+            expect(level.id_country).toBe(39);
+        });
+    });
+
+    test("Should respond with 400 if country ID format is invalid", async () => {
+        const response = await request(server)
+            .get("/api/v1/getEducationalLevel")
+            .query({ id_country: "abc" });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("error", "Country ID format is invalid");
+    });
+
+    test("Should respond with 404 if no educational levels exist for the provided country", async () => {
+        const response = await request(server)
+            .get("/api/v1/getEducationalLevel")
+            .query({ id_country: 999 });
+
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toHaveProperty("error", "There are no educational levels available for this country ID");
+    });
+});
 
 
 
